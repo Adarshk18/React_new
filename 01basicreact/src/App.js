4c4737a0b7f264d2b0853743ec0f351d87560c34
FOODOHOLIC <adarshkohli183@gmail.com>
2024-11-23 19:43:13 +0530

    commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+function App() {
+  return (
+    <h1>Learning React | Adarsh Sharma</h1>
+
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Demo from "./demo"
+
+function App() {
+
+  return (
+    <>
+    <Demo/>
+    <h3>test paragraph</h3>
+    </>
+    
+  )
+}
+
+export default App
