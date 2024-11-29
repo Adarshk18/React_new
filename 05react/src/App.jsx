31b598a679fa561381892cf4485f1c1bc31afb1e
FOODOHOLIC <adarshkohli183@gmail.com>
2024-11-29 23:49:49 +0530

    Added initial files for 05react project

---
+++ 05react/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useCallback } from 'react'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed,setNumberAllowed] = useState(false)
+  const [charAllowed,setCharAllowed] = useState(false)
+  const [password,setPassword] = useState("")
+
+
+   // useCallback is a hook that lets us cache a function definition between re-renders.
+
+  const passwordGenerator = useCallback(() =>{
+    let pass = "";
+    let String ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(numberAllowed) String += "0123456789"
+    if(charAllowed) String += "!@#$%^&+=_+=[]{}~`"
+
+    for (let i=1; i<=array.length; i++ ) {
+      let char = Math.floor(Math.random()*String.length+1);
+      pass = String.charAt(char);
+    }
+
+    setPassword(pass)
+
+  },[length,numberAllowed,charAllowed,setPassword]);
+   
+  
+
+  return (
+    <>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className="flex-shadow rounded-lg overflow-hidden mb-4">
+          <input type='text'
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Password'
+          readOnly
+          />
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+          
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
