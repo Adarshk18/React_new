c8c831e7446e9eb0def7d0849f4d0af5fa9b13b8
FOODOHOLIC <adarshkohli183@gmail.com>
2024-11-30 18:04:38 +0530

    Update: Added new files and modified existing ones

--- 05react/src/App.jsx
+++ 05react/src/App.jsx
@@ -1,54 +1,98 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed,setNumberAllowed] = useState(false)
-  const [charAllowed,setCharAllowed] = useState(false)
-  const [password,setPassword] = useState("")
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
+  //ref hook
+  const passwordRef = useRef(null)
 
-   // useCallback is a hook that lets us cache a function definition between re-renders.
+  // useCallback is a hook that lets us cache a function definition between re-renders.
 
-  const passwordGenerator = useCallback(() =>{
+  const passwordGenerator = useCallback(() => {
     let pass = "";
-    let String ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if(numberAllowed) String += "0123456789"
-    if(charAllowed) String += "!@#$%^&+=_+=[]{}~`"
+    if (numberAllowed) String += "0123456789";
+    if (charAllowed) String += "!@#$%^&+=_+=[]{}~`";
 
-    for (let i=1; i<=array.length; i++ ) {
-      let char = Math.floor(Math.random()*String.length+1);
-      pass = String.charAt(char);
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * String.length + 1);
+      pass += String.charAt(char);
     }
 
-    setPassword(pass)
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
-  },[length,numberAllowed,charAllowed,setPassword]);
-   
-  
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,100);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
-        <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
         <div className="flex-shadow rounded-lg overflow-hidden mb-4">
-          <input type='text'
-          value={password}
-          className='outline-none w-full py-1 px-3'
-          placeholder='Password'
-          readOnly
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
           />
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
-          
+          <button onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
         </div>
-        <div className='flex text-sm gap-x-2'>
-          <div className='flex items-center gap-x-1'>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
 
+            <label>Length: {length}</label>
           </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="charInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="charInput">Characters</label>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
