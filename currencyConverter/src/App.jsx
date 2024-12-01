78cef941638ee3d1a7fb91e10bec4128d5bf417f
FOODOHOLIC <adarshkohli183@gmail.com>
2024-12-01 21:25:48 +0530

    Update: Added new files and modified existing ones

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { InputBox } from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+import './App.css'
+
+function App() {
+
+  const [amount,setAmount] = useState(0)
+  const [from,setFrom] = useState("USD")
+  const [to,setTo] = useState("INR")
+  const [convertedAmount , setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
