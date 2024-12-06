1a39afd070ab81edc5b924e3c2afae846ab7b159
FOODOHOLIC <adarshkohli183@gmail.com>
2024-12-06 20:59:58 +0530

    Update: Added new files and modified existing ones

--- toDo/src/App.jsx
+++ toDo/src/App.jsx
@@ -1,16 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { ToDoProvider } from "./contexts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello World!</h1>
-    </>
-  )
+    <ToDoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <form className="flex">
+        <input
+          type="text"
+          placeholder="Write Todo..."
+          className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        />
+        <button
+          type="submit"
+          className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+        >
+          Add
+        </button>
+      </form>
+    </ToDoProvider>
+  );
 }
 
-export default App
+export default App;
